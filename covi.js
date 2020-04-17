46db7a47319aa4c3d65cc9b481205a79429f91f4
Phillip <phillip7.et@gmail.com>
2020-04-17 22:44:53 +0700

    clean code for reading easier

--- covi.js
+++ covi.js
@@ -34,7 +34,7 @@ function genFileName(extentionName, hasHourSuffix) {
     return `${y}${m >= 10 ? m : '0' + m}${day >= 10 ? day : '0' + day}${hasHourSuffix ? '_' + d.getHours() + 'h' + d.getMinutes() + 'm' : ''}${extentionName}`
 }
 
-async function fetchHtmlTable(url) {
+async function fetchContentPage(url) {
     let s = new Date()
     log('%s: fetching data...', s.toLocaleString())
     let bodyHtml = await rp(url)
@@ -54,60 +54,58 @@ async function fetchHtmlTable(url) {
     log('Total seconds: %ss', Math.floor((e.getTime() - s.getTime()) / 1000))
     return bodyHtml
 }
-function getHtmlTableWorldToJson(htmlBody) {
+function genHtmlToJsonOfTheWorld(htmlBody) {
     let $ = cheerio.load(htmlBody)
     let rows = $('#main_table_countries_today tr[class="total_row_world"]')
-    let row = cheerio.load(rows.eq(0).html().trim(), { xmlMode: true })
-    let jsonTheWorld = genHtmlRowTableToArray(row.html().trim())
+    let rowOfTheWorld = cheerio.load(rows.eq(0).html().trim(), { xmlMode: true })
+    let jsonTheWorld = genHtmlToJsonOfCountry(rowOfTheWorld.html().trim())
     return jsonTheWorld
 }
-async function genHtmlTableToJson(htmlBody) {
+async function genContentToJson(htmlBody) {
     let jsonCountries = {}
     let jsonCountry = {}
     let $ = cheerio.load(htmlBody)
-    let jsonTheWorld = getHtmlTableWorldToJson(htmlBody)
+    let jsonTheWorld = genHtmlToJsonOfTheWorld(htmlBody)
     jsonCountries = Object.assign(jsonCountries, jsonTheWorld)
     let rows = $('#main_table_countries_today tr[style=""]')
     for (let i = 0; i < rows.length; i++) {
         let row = cheerio.load(rows.eq(i).html().trim(), { xmlMode: true })
-        jsonCountry = genHtmlRowTableToArray(row.html().trim())
+        jsonCountry = genHtmlToJsonOfCountry(row.html().trim())
         jsonCountries = Object.assign(jsonCountries, jsonCountry)
     }
     let content = JSON.stringify(jsonCountries)
     //log(content)
-    await writeFile(dataPath + genFileName('.json'), content)
-    if (!isLiveHeroku) await writeFile(dataPath + genFileName('.json', true), content)
     return content
 }
-const delComma = (selectorRow, i) => selectorRow.eq(i).text().trim().replace(/,+/g, '')
-function genHtmlRowTableToArray(strHtmlRow) {
+const getCaseNumber = (selectorRow, i) => selectorRow.eq(i).text().trim().replace(/,+/g, '')
+function genHtmlToJsonOfCountry(strHtmlRow) {
     let $ = cheerio.load(strHtmlRow, { xmlMode: true })
     let jsonRow = {},
         // name country
         rowKeyName = $('td').eq(0).text().trim().replace(/\s+|\n|-|\./g, '_').replace('__', '_').toLowerCase(),
         rowKeyValue = [
             //totalCases: 
-            +delComma($('td'), 1),
+            +getCaseNumber($('td'), 1),
             //newCases: 
-            +delComma($('td'), 2),
+            +getCaseNumber($('td'), 2),
             //totalDeaths: 
-            +delComma($('td'), 3),
+            +getCaseNumber($('td'), 3),
             //newDeaths: 
-            +delComma($('td'), 4),
+            +getCaseNumber($('td'), 4),
             //totalRecovered: 
-            +delComma($('td'), 5),
+            +getCaseNumber($('td'), 5),
             //activeCases: 
-            +delComma($('td'), 6),
+            +getCaseNumber($('td'), 6),
             //seriousCritical: 
-            +delComma($('td'), 7),
+            +getCaseNumber($('td'), 7),
             //totalCasesPer1MPop: 
-            +delComma($('td'), 8),
+            +getCaseNumber($('td'), 8),
             //deathsPer1MPop: 
-            +delComma($('td'), 9),
+            +getCaseNumber($('td'), 9),
             //totalTests: 
-            +delComma($('td'), 10),
+            +getCaseNumber($('td'), 10),
             //testsPer1MPop: 
-            +delComma($('td'), 11),
+            +getCaseNumber($('td'), 11),
         ]
     jsonRow[rowKeyName] = rowKeyValue
     return jsonRow
@@ -116,8 +114,12 @@ const WAIT_NEXT_FETCHING = 900 * 1000 // 15 minutes
 async function run() {
     let currentData = []
     try {
-        let html = await fetchHtmlTable('https://www.worldometers.info/coronavirus/')
-        currentData = await genHtmlTableToJson(html)
+        let content = await fetchContentPage('https://www.worldometers.info/coronavirus/')
+        currentData = await genContentToJson(content)
+
+        await writeFile(dataPath + genFileName('.json'), currentData)
+        if (!isLiveHeroku) await writeFile(dataPath + genFileName('.json', true), currentData)
+
         log('%s: waiting after %ss', new Date().toLocaleString(), WAIT_NEXT_FETCHING)
         setTimeout(async () => await run(), WAIT_NEXT_FETCHING)
         return currentData
