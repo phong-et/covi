b7d74920cd72e4433db80db9a727ce6072e6e1a4
Phillip <phillip7.et@gmail.com>
2020-04-24 22:37:06 +0700

    send a request to php service to save latest data

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,9 +1,16 @@
 (function () {
+    function genFileName(extentionName, hasHourSuffix) {
+        let d = new Date(),
+            y = d.getFullYear(),
+            m = (d.getMonth() + 1),
+            day = d.getDate()
+        return `${y}${m >= 10 ? m : '0' + m}${day >= 10 ? day : '0' + day}${hasHourSuffix ? '_' + d.getHours() + 'h' + d.getMinutes() + 'm' : ''}${extentionName}`
+    }    
     function fetch() {
         $.ajax({
             url: 'fetch.php',
             type: 'GET',
-            data: { cmd: 'get-latest-data' },
+            data: { date: genFileName('.json', true) },
             success: function (isOpen) {
                 try {
                     console.log(isOpen)
