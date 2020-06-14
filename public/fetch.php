36fd7105ecebff6d2d9ea3f5f0e163fd6c59e9df
Phillip <phillip7.et@gmail.com>
2020-06-14 07:39:12 +0700

    add fetching ytd data

--- public/fetch.php
+++ public/fetch.php
@@ -1,8 +1,10 @@
 <?php
-    @$date = $_GET["date"];
-    if($date)
+    $date = $_GET["date"];
+    $yesterday = $_GET["yesterday"];
+    $url = $yesterday == "yesterday" ? "http://covichart.herokuapp.com/yesterday" : "http://covichart.herokuapp.com/latest"
+    if($date) 
     {
-        $ch = curl_init("http://covichart.herokuapp.com/latest");
+        $ch = curl_init($url);
         $fp = fopen("data/$date", "w");
 
         curl_setopt($ch, CURLOPT_FILE, $fp);
