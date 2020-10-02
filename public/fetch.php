62dddc47ae565c0c8cae51a42b2666b14cc5667a
Phillip <phillip7.et@gmail.com>
2020-10-02 21:37:25 +0700

    send json file to host

--- public/fetch.php
+++ public/fetch.php
@@ -1,22 +1,12 @@
 <?php
-    $date = $_GET["date"];
-    $yesterday = $_GET["yesterday"];
-    $url = $yesterday == "yesterday" ? "http://covichart.herokuapp.com/yesterday" : "http://covichart.herokuapp.com/latest";
+    $date = $_POST["date"];
+    $data =  $_POST["data"];
+    echo $date . "-";
     if($date) 
     {
-        $ch = curl_init($url);
-        $fp = fopen("data/$date", "w");
-
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-
-        curl_exec($ch);
-        if(curl_error($ch)) {
-            fwrite($fp, curl_error($ch));
-        }
-        curl_close($ch);
-        fclose($fp);
-        echo $url;
+        $file = fopen("data/$date", "w");
+        echo fwrite($file, $data);
+        fclose($file);
     }
     else
         echo "Miss date param"
